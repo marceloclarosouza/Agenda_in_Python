70be54d69caa6c6d7e3a314b06ecbe2cc7986a19
MarceloClaroSouza <marcelo.claro.souza@gmail.com>
2020-08-20 18:06:51 -0300

    problems to itherate the dictionary

---
+++ AgendaContatos/AgendaContatos.py
@@ -0,0 +1,26 @@
+
+
+
+
+class Agenda():
+    def __init__(self, name, age, phone, email):
+        self.name = name
+        self.age = age
+        self.phone = phone
+        self.email = email
+        self.agenda = []
+        self.contacts = {}
+
+    def insert_contacts(self):
+        contacts[0] = name
+        contacts[1] = age
+        contacts[2] = phone
+        contacts[3] = email
+        agenda.append(contacts)
+        return self.agenda
+
+    #def sort_agenda(self):
+        #agenda.sort([name for name in agenda])
+
+print(Agenda("marcelo", "37", 54654654654, "a@a.com.br"))
+
