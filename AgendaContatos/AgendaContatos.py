661a64d5af8ca4231db0b790c072638d9e16b2cb
MarceloClaroSouza <marcelo.claro.souza@gmail.com>
2020-09-06 13:12:03 -0300

    roda mas não printa

--- AgendaContatos/AgendaContatos.py
+++ AgendaContatos/AgendaContatos.py
@@ -1,25 +1,22 @@
 """Agenda de contatos usando OOP. Quero armazenar os contatos em uma lista de dicionarios"""
 
-
 class Agenda():
-    def __init__(self, name, age, phone, email):
-        self.name = name
-        self.age = age
-        self.phone = phone
-        self.email = email
-        self.agenda = []
-        self.contacts = {}
+    def __init__(self):
+        self.contacts={}
+        self.agenda=[]
 
-    def dicionario(self):
-        self.contacts['name'] = self.name
-        self.contacts['age'] = self.age
-        self.contacts['phone'] = self.phone
-        self.contacts['email'] = self.email
-        self.agenda.append(contacts)  
-        
-    def __str__(agenda):
-        return self.agenda
+    def dicionario(self, name, age, phone, email):
+        self.contacts["name"] = name
+        self.contacts["age"] = age
+        self.contacts["phone"] = phone
+        self.contacts["email"] = email
+        self.agenda.append(self.contacts)
 
-    
-data = Agenda("marcelo", "37", 54654654654, "a@a.com.br")
-print(f'Contatos: {agenda}')
+    def __str__(self):
+        print(self.agenda)
+
+
+data = Agenda()
+data.dicionario("mcs", 2131, "hlkhlh", "kljlkjl")
+
+
