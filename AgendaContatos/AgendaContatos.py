0693478e46c02f75be2e19f38d9d712b53e8983b
MarceloClaroSouza <marcelo.claro.souza@gmail.com>
2020-09-11 10:20:48 -0300

    está salvando apenas o ultimo contato de forma duplicada

--- AgendaContatos/AgendaContatos.py
+++ AgendaContatos/AgendaContatos.py
@@ -8,16 +8,16 @@ class Agenda():
         self.agenda=[]
 
     def dicionario(self, name, age="None", phone="None", email="None"):
-        if name not in self.contacts:
+        #if name not in self.contacts:
             self.contacts["name"] = name
             self.contacts["age"] = age
             self.contacts["phone"] = phone
             self.contacts["email"] = email
             self.agenda.append(self.contacts)
-            with open("agenda_dados.txt", "a") as arquivo:
+            with open("agenda_dados.txt", "w") as arquivo:
                 arquivo.write(json.dumps(self.agenda))
-        else:
-            print("Este contato já foi cadastrado no sistema")
+        #else:
+            #print("Este contato já foi cadastrado no sistema")
 
 
     #def salva_retorna_agenda(self):
@@ -25,14 +25,14 @@ class Agenda():
             #arquivo.write(json.dumps(self.agenda))
     def retorna_agenda(self):
         with open("agenda_dados.txt", "r") as arquivo:
-            aberto = json.loads(arquivo.read())
+            aberto = arquivo.read()
             return aberto
    
 
 data = Agenda()
 
 data.dicionario(input("Nome: "), input("age: "), input("phone: "), input("email: "))
-#data.dicionario(input("Nome: "), input("age: "), input("phone: "), input("email: "))
+data.dicionario(input("Nome: "), input("age: "), input("phone: "), input("email: "))
 #data.dicionario(input("Nome: "), input("age: "), input("phone: "), input("email: "))
 
 print(data.retorna_agenda())
