fdba1dd157d52b1a4ff8bff64202210bc82bd5fd
MarceloClaroSouza <marcelo.claro.souza@gmail.com>
2020-09-12 09:36:34 -0300

    piorou

--- AgendaContatos/AgendaContatos.py
+++ AgendaContatos/AgendaContatos.py
@@ -8,31 +8,37 @@ class Agenda():
         self.agenda=[]
 
     def dicionario(self, name, age="None", phone="None", email="None"):
-        #if name not in self.contacts:
+        if name not in self.contacts:
             self.contacts["name"] = name
             self.contacts["age"] = age
             self.contacts["phone"] = phone
             self.contacts["email"] = email
-            self.agenda.append(self.contacts)
-            with open("agenda_dados.txt", "w") as arquivo:
-                arquivo.write(json.dumps(self.agenda))
-        #else:
-            #print("Este contato já foi cadastrado no sistema")
-
-
-    #def salva_retorna_agenda(self):
-        #with open("agenda_dados.txt", "w") as arquivo:
-            #arquivo.write(json.dumps(self.agenda))
+            #self.agenda.append(self.contacts)
+        else:
+            print("Este contato já foi cadastrado no sistema")
+            
+            
+    def salvar(self):
+            with open("agenda_dados.txt", "a") as arquivo:
+                #arquivo.write(json.dumps(self.agenda))
+                arquivo.write(json.dumps(self.contacts))
+                
+  
     def retorna_agenda(self):
         with open("agenda_dados.txt", "r") as arquivo:
             aberto = arquivo.read()
-            return aberto
+            for dic in aberto:
+                self.agenda.append(dic)
+            data.salvar(self.agenda)    
+            return self.agenda
    
 
 data = Agenda()
 
 data.dicionario(input("Nome: "), input("age: "), input("phone: "), input("email: "))
+data.salvar()
 data.dicionario(input("Nome: "), input("age: "), input("phone: "), input("email: "))
+data.salvar()
 #data.dicionario(input("Nome: "), input("age: "), input("phone: "), input("email: "))
 
 print(data.retorna_agenda())
