b240d143c4386e2a28ac3f11eeda57b406b80e4b
MarceloClaroSouza <marcelo.claro.souza@gmail.com>
2020-09-16 20:53:58 -0300

    working good

--- AgendaContatos/AgendaContatos.py
+++ AgendaContatos/AgendaContatos.py
@@ -6,14 +6,12 @@ class Agenda():
     def __init__(self):
         """Inicia a lista e o dicionário utilizados na classe Agenda"""
         self.contacts={}
-        #self.agenda=[]
+        self.agenda=[]
 
     def criar_agenda(self):
         """Cria a agenda de contatos"""#deve ser executada uma única vez
         with open("agenda_dados.txt", "w") as arquivo:
-            arquivo.write(json.dumps(""))
-        pass
-
+            pass
 
     def dicionario(self, name, age="None", phone="None", email="None"):
         """Adiciona os contatos a agenda"""
@@ -24,18 +22,18 @@ class Agenda():
         with open("agenda_dados.txt", "a") as arquivo:
             arquivo.write(json.dumps(self.contacts))
 
-
     def retorna_agenda(self):
         """retorna a aganda"""
         with open("agenda_dados.txt", "r") as arquivo:
             aberto = arquivo.read()
-            return aberto
-
+            self.agenda.append(aberto)
+            return self.agenda
 
 data = Agenda()
 data.criar_agenda()
 
-for i in range (5):
+for i in range (3):
     data.dicionario(input("Nome: "), input("age: "), input("phone: "), input("email: "))
 
 print(data.retorna_agenda())
+
